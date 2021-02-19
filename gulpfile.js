9b8b6c780d7a7a2eac78e272d088c3126d36056d
Josh Minnie <josh.minnie@gmail.com>
2021-02-19 17:50:04 -0500

    Simplify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,22 @@
-const { src, dest, series, parallel } = require('gulp');
+const { src, dest, series } = require('gulp');
 
 var concat = require('gulp-concat');
 var del = require('del');
-var fs = require('fs');
-var path = require('path');
 
-var wishlists = 'wishlists';
+function clean(done) {
+  del('wishlist.txt');
 
-function clean(cb) {
-  del('wishlists/*.txt');
-  cb();
-}
-
-function getFolders(directory) {
-  return fs.readdirSync(directory)
-    .filter(function(file) {
-      return fs.statSync(path.join(directory, file)).isDirectory();
-    });
+  done();
 }
 
 function build(done) {
-  var folders = getFolders(wishlists);
-  if (folders.length === 0) { return done(); }
-
-  folders.map(function(folder) {
-    return src(path.join(wishlists, folder, '/**/*.txt'))
-      .pipe(concat(folder + '.txt'))
-      .pipe(dest(wishlists));
-  });
-
-  done();
-}
-
-function compile(done) {
-  src(path.join(wishlists, '*.txt'))
-    .pipe(concat('wishlist.txt'))
+  src('./wishlists/**/*.txt')
+    .pipe(concat({ path: 'wishlist.txt' }))
     .pipe(dest('./'));
+
   done();
 }
 
-exports.compile = compile; // TODO: Make this part of the default task, the series doesn't wait for it to complete currently.
 exports.default = series(clean, build);
+exports.clean = clean;
+exports.build = build;
